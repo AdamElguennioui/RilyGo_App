0376b0a967afdb55ac3200137fe69e136b3f58a1
Adam <adam.elguennioui@gmail.com>
2026-07-29 19:08:32 +0200

    debug: temporary boot-error overlay to diagnose production-only crash

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1338455161" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "43492544" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
